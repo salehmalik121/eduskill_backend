363145ed77c07ef1a030012fbfc9608fd5857fa8
salehmalik121 <salehmalik121@gmail.com>
2023-10-22 05:52:14 -0700

    fixes

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ mongoose.connect("mongodb+srv://salehmalik121:pKQqiyYUtcGvETfv@cluster0.oodu1bc.
 })
 //server instance 
 const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use(cors());
 
 
